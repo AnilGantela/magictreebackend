9f9537bce9b65c1fb796d2b75013dec197598a22
unknown <anilkumar.gantela77@gmail.com>
2025-05-05 15:19:14 +0530

    sub categories

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,6 +14,6 @@ router.post(
 
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
-router.get("/:subcategory", getProductsBySubcategory);
+router.get("/:subcategory", productController.getProductsBySubcategory);
 
 module.exports = router;
