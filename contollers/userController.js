00348c8a555162bd6ef2d9224f290cb70ae91602
unknown <anilkumar.gantela77@gmail.com>
2025-05-06 18:48:07 +0530

    review

--- contollers/userController.js
+++ contollers/userController.js
@@ -152,7 +152,8 @@ const removeAddress = async (req, res) => {
 
 const getAllAddresses = async (req, res) => {
   try {
-    const user = await User.findById(req.userId);
+    const userId = req.user._id;
+    const user = await User.findById(userId);
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
